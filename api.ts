b44c53dcaa80d6ef4d13c8850a8161894a866b91
Maria Redman <absmcc@gmail.com>
2022-08-22 13:32:26 -0400

    loaded-docker

--- api.ts
+++ api.ts
@@ -7,12 +7,12 @@ app.use(cors())
 const port = 8000;
 
 app.get("/",(req: Request, res: Response) => {
-    res.status(200).send("Express in docker is working...And now with logs")
+    res.status(200).send("Docker is working...And now with logs")
 })
 
 app.get("/todo",(req: Request, res: Response) => {
     const todo = ["take out garbage", "do dishes", "go shopping for a new iphone"]
-    res.status(200).json(todo)
+    res.status(200).json(love)
 })
 
 app.listen(port,()=> {
